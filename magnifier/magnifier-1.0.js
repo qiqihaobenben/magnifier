2c61ddefdfef0d6f28f0a39c39860e1a973d8591
cfangxu <chenfangxu_qixin@163.com>
2017-03-07 16:46:48 +0800

    change file

--- magnifier/magnifier-1.0.js
+++ magnifier/magnifier-1.0.js
@@ -2,9 +2,10 @@
  * Created by cfangxu on 2017/2/22.
  * 商城商品详情页放大镜的插件
  * 依赖jquery >=1.7
+ * 兼容性IE >= 9
  * @multiple 可放大的倍数，默认是2倍
  * @bgColor 暂时只能设置rgba的颜色
- * @canShow 如果因为加载等原因不想鼠标移入还有效果，可以通过给当前元素增加data-show的属性，
+ * @canShow 如果因为加载等原因想暂时禁止鼠标的移入事件，可以通过给当前元素下的img节点增加data-show的属性，
  * 当属性值为false时，移入效果终止，知道data-show属性更改为非false等其他值。也可以一开始就设置false来禁止鼠标移入效果
  * @time 设置延迟时间，避免误触发，提高用户体验
  */
